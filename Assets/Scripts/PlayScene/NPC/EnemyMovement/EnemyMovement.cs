a0549e3fb9292f9185a3fea459c5e6ff646f326a
Frederick <fchapdelaine@gmail.com>
2024-01-13 12:37:45 -0500

    Added waypoints and enemy movement

---
+++ Assets/Scripts/PlayScene/NPC/EnemyMovement/EnemyMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 200f;
+    private Transform target;
+    private int wavepointIndex = 0;
+    void Start ()
+    {
+        target = Waypoints.points[0];
+    }
+
+    void Update ()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaypoint();
+        }
+    }
+    void GetNextWaypoint ()
+    {
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
